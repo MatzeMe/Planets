1a0a784243da83988df938460df46cf44e36e7c3
sieben <sieben@192.168.0.16>
2015-05-26 18:40:24 +0200

    keine Änderung

--- Planets_0.1/src_test/Production_Test.js
+++ Planets_0.1/src_test/Production_Test.js
@@ -65,7 +65,6 @@ AsyncTestCase("Production_Test", {
 		queue.call('Step 3: Update() wird in 11,0 Sekunden aufgerufen.', function(callbacks) {
 		    var myCallback = callbacks.add(function() {
 		    	TestProduction.Update();
-		
 		    });
 		    window.setTimeout(myCallback, 2000);
 		 });
