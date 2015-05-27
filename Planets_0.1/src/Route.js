da734f75d1203ffa378b81df17da4ac893da8a38
Mathias Meusel <Mathias.Meusel@gmx.de>
2015-05-27 20:29:44 +0200

    Route und Travel hinzugefügt

--- Planets_0.1/src/Route.js
+++ Planets_0.1/src/Route.js
@@ -1,13 +1,53 @@
-function Route(StartA, TargetA){ 
+function Route(startA, targetA, groupA){ 
 	
-	this.Start = StartA;
-	this.Target = TargetA;
-	this.Travelers = [];
-	this.distance;				//Aus Planetenkoordinaten berechnen, Formel siehe Gamecontroler.js
+	this.start = startA;
+	this.target = targetA;
+	this.travelers = []; 
+	this.distance = GameControler.sum;				//Aus Planetenkoordinaten berechnen, Formel siehe Gamecontroler.js
+	this.remainingDistance = distance;
+	this.travelStart = Date.now();
 	
-	this.Update = function(){
+	this.updateTravel = function(travelA)
+	{ 
+		//!!!! In jeder Route geht die eigene Update() ihre Travel-objekte durch und fragt deren Update ab
+		this.travel = travelA; 
+
+		for(int i = 0; i<travelers.length; i++)
+		{
+			if(updateTravel == true)
+			{
+				transferGroupToPlanet(travelers[i], target);
+				endTravel(travelers[i]);
+			}
+		} 
+		
+	} 
+	  
+	
+	this.startTravel = function(groupA)	// Erzeug das Travel-Objekt und  pusht die Traveler-Gruppe in das Array
+	{  
+		this.group = groupA;
+		travelers.push(new Travel(group, target, distance)); 
+	}
+	
+	this.transferGroupToPlanet = function(group, target) // Travelzeit ist um. Gruppe wird in Array des Zielplaneten geschrieben.
+	{
+		this.target.presentGroups.push(group);
+		
+	}
+	
+	
+	
+	this.endTravel = function(travel)  // Löschen des Travel-Objektes aus dem traveler-Array.
+	{
+		this.travelers.splice(this.travelers.indexof(travel.presentGroup),1);
+	}
 		
 	
-	}
+
+//	this.calculateDistance = function()  // Das macht der GameController
+//	{
+//		
+//	}
 	
 }
---
+++ Planets_0.1/src/Travel.js
@@ -0,0 +1,30 @@
+function Travel(groupA, targetA, distanceA)
+{
+	this.distance = distanceA;	//Zahl
+	this.presentGroup = groupA;	//Group-Objekt
+	this.target = targetA;	//Planet-Objekt
+	this.targetReached = false;	//bool
+	this.travelTime:			//float/millis 
+	this.remainingTravelTime;
+	this.travelStart = Date.now();
+		
+	
+	this.calculateTravelTime = function() //Berechnet aus Entfernung und Geschwindigkeit der Gruppe die Reisezeit
+	{
+		this.travelTime = (distance / presentGroup.speed);
+	}
+	
+	
+	this.updateTravel = function() // Überprüft, ob die Reisezeit abgelaufen ist. Wenn ja -> true. Sonst false.
+	{
+		this.remainingTravelTime = this.travelTime - (Date.now() - this.travelStart);
+		if(this.remainingTravelTime <= 0)
+			{
+				this.remainingTravelTime = 0;
+				return true;
+			} 
+		else
+			return false;
+	}
+		  
+} 
