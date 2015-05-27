2e65b9b18664e5d61b141f24ebf68d654d32fc66
sieben <sieben@192.168.0.16>
2015-05-27 12:21:53 +0200

    Neu erstellt

--- Planets_0.1/src/GameControler.js
+++ Planets_0.1/src/GameControler.js
@@ -5,7 +5,7 @@ function GameControler(UniverseA, PlayersA){
 	this.GameOver = false;
 	this.FrameRate = 30;
 
-	this Milkyways = [];
+	this.Milkyways = [];
 	
 	for(var i = 0; i < this.Universe.length; i++){
 		for(var o = 0; o < this.Universe.length; o++){
@@ -14,12 +14,38 @@ function GameControler(UniverseA, PlayersA){
 				var x = this.Universe[o].x - this.Universe[i].x;
 				var y = this.Universe[o].y - this.Universe[i].y;
 				
-				var sum = sqrt((x*x)+(y*y));
-				
-				if(sum >= this.Universe[i].TravelRadius){
-					this.Milkyways.push(new Route(this.Universe[i],  this.Universe[o]));
+				var sum = Math.sqrt((x*x)+(y*y));
+
+				if(sum <= this.Universe[i].TravelRadius){
+					this.Milkyways.push(new Route(this.Universe[i],  this.Universe[o])); 
 				}				
 			}
 		}		
 	}
+	
+	this.Update = function(){
+		
+		
+		for(var i = 0; i < this.Universe.length; i++ ){
+			this.Universe[i].Update();			
+		}
+		
+		this.GameOver = true;
+		for(var i = 0; i < this.Universe.length -1; i++ ){
+			if(this.Universe[i].Owner.ID != this.Universe[i+1].Owner.ID){
+				this.GameOver = false;
+			}			
+		}
+		
+		for(var i = 0; i < this.Milkyways.length; i++ ){
+			this.Milkyways[i].Update();
+		}
+		
+		if(this.GameOver == true){
+			//Tu Was, das Spiel ist aus, mach dem ganzen eine Ende, los, tue es!
+		}
+			
+	}
+	
+	
 }
--- Planets_0.1/src_test/GameControler_Test.js
+++ Planets_0.1/src_test/GameControler_Test.js
@@ -16,9 +16,9 @@ TestCase("GameControler_Test", {
 	 */	
 	
 
-"test Initialisierung / Gruen wenn Gamecontroler korrekt initialisiert": function() {  
+"test Initialisierung / Gruen wenn Gamecontroler korrekt initialisiert": function() {   
 	
-		var TestUniverse = [new Planet(10, 350, 350), new Planet(10, 400, 350)];
+		var TestUniverse =[new Planet(10, 350, 350), new Planet(10, 400, 350)];
 		var TestPlayer=[new Player(1), new Player(2)] 
 		
 		TestUniverse[0].setOwner(TestPlayer[0]);
@@ -30,25 +30,93 @@ TestCase("GameControler_Test", {
 		assertInstanceOf("Planet", Planet, TestGC.Universe[1]);
 		
 		assertInstanceOf("Route", Route, TestGC.Milkyways[0]);
-		assertNotInstanceOf("Route", Route, TestGC.Milkyways[1]);
+		assertInstanceOf("Route", Route, TestGC.Milkyways[1]); 
 		
-		assertInstanceOf("Player", Player, TestGC.Milkyways[0]);
-		assertInstanceOf("Player", Player, TestGC.Milkyways[1]);
+		assertInstanceOf("Player", Player, TestGC.Players[0]); 
+		assertInstanceOf("Player", Player, TestGC.Players[1]);
 		
-		assertFalse("GameOver", TestGC.GameOver);
+		assertFalse("GameOver", TestGC.GameOver); 
 		
 		assertNumber("FrameRate", TestGC.FrameRate);
 	},  
 	
 "test Verbindung / Gruen wenn 2 Planeten durch genau 2 Routen verbunden": function() {  //2Routen, weil Routen Einbahnstrassen sein können
 		
-		var TestUniverse = [new Planet(10, 350, 350), new Planet(10, 400, 350)];
+		var TestUniverse = [new Planet(10, 350, 350), new Planet(10, 400, 350)];	//TravelRadius momentan: Mass * 30
 		var TestPlayer=[new Player(1), new Player(2)] 
 		
 		TestUniverse[0].setOwner(TestPlayer[0]);
 		TestUniverse[1].setOwner(TestPlayer[1]);
 	
 		var TestGC = new GameControler(TestUniverse, TestPlayer);
+		
+		assertInstanceOf("Route", Route, TestGC.Milkyways[0]);
+		assertInstanceOf("Route", Route, TestGC.Milkyways[1]); 
+		assertNotInstanceOf("Route", Route, TestGC.Milkyways[2]); 
+		
+		assertEquals("Start1", TestGC.Universe[0], TestGC.Milkyways[0].Start);
+		assertEquals("Target1", TestGC.Universe[1], TestGC.Milkyways[0].Target);
+		
+		assertEquals("Start2", TestGC.Universe[1], TestGC.Milkyways[1].Start);
+		assertEquals("Target2", TestGC.Universe[0], TestGC.Milkyways[1].Target);
+		
+	},  
+	
+	
+"test Verbindung / Gruen wenn 2 Planeten durch genau 1 Route verbunden wegen unterschiedlichem TravelRadius": function() {  //2Routen, weil Routen Einbahnstrassen sein können
+		
+		var TestUniverse = [new Planet(10, 200, 350), new Planet(5, 400, 350)];	//TravelRadius momentan: Mass * 30: Planet1 -> 300, Planet2 -> 150
+		var TestPlayer=[new Player(1), new Player(2)] 
+		
+		TestUniverse[0].setOwner(TestPlayer[0]);
+		TestUniverse[1].setOwner(TestPlayer[1]);
+	
+		var TestGC = new GameControler(TestUniverse, TestPlayer);
+		
+		assertInstanceOf("Route", Route, TestGC.Milkyways[0]); 
+		assertNotInstanceOf("Route", Route, TestGC.Milkyways[1]); 
+		
+		assertEquals("Start1", TestGC.Universe[0], TestGC.Milkyways[0].Start);
+		assertEquals("Target1", TestGC.Universe[1], TestGC.Milkyways[0].Target);		
+	},  
+	
+"test Sieg / Gruen wenn Kein Sieg festgestellt wird, solange nicht alle Planeten einem Spieler gehören": function() {  //2Routen, weil Routen Einbahnstrassen sein können
+		
+		var TestUniverse = [new Planet(10, 200, 350), new Planet(5, 400, 350), new Planet(5, 400, 350)];	//TravelRadius momentan: Mass * 30: Planet1 -> 300, Planet2 -> 150
+		var TestPlayer=[new Player(1), new Player(2)] 
+		
+		TestUniverse[0].setOwner(TestPlayer[0]);
+		TestUniverse[1].setOwner(TestPlayer[1]);
+	
+		var TestGC = new GameControler(TestUniverse, TestPlayer);
+		
+		TestGC.Update();
+		assertFalse(TestGC.GameOver);	
+		
+		TestUniverse[1].setOwner(TestPlayer[0]);
+		
+		TestGC.Update();
+		assertFalse(TestGC.GameOver);
+		
+	},  
+	
+"test Sieg / Gruen wenn Sieg festgestellt wird, wenn alle Planeten einem Spieler gehören": function() {  //2Routen, weil Routen Einbahnstrassen sein können
+		
+		var TestUniverse = [new Planet(10, 200, 350), new Planet(5, 400, 350)];	//TravelRadius momentan: Mass * 30: Planet1 -> 300, Planet2 -> 150
+		var TestPlayer=[new Player(1), new Player(2)] 
+		
+		TestUniverse[0].setOwner(TestPlayer[0]);
+		TestUniverse[1].setOwner(TestPlayer[1]);
+	
+		var TestGC = new GameControler(TestUniverse, TestPlayer);
+		
+		TestGC.Update();
+		assertFalse(TestGC.GameOver);
+		
+		TestUniverse[1].setOwner(TestPlayer[0]);
+		
+		TestGC.Update();
+		assertTrue(TestGC.GameOver);
 		
 	},  
 	
