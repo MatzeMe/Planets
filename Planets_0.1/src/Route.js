5395d266c746725e6b9535496688a4d24f94fb45
sieben <sieben@192.168.0.16>
2015-05-27 12:21:25 +0200

    Stub

---
+++ Planets_0.1/src/Route.js
@@ -0,0 +1,13 @@
+function Route(StartA, TargetA){ 
+	
+	this.Start = StartA;
+	this.Target = TargetA;
+	this.Travelers = [];
+	this.distance;				//Aus Planetenkoordinaten berechnen, Formel siehe Gamecontroler.js
+	
+	this.Update = function(){
+		
+	
+	}
+	
+}
